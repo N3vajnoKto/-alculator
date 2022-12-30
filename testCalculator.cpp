096793bb335166bd5924e7f791db7cd6fe7a9191
N3vajnoKto <kzarovskiy635@gmail.com>
2022-12-30 20:13:39 +0300

    fixed tests

--- testCalculator.cpp
+++ testCalculator.cpp
@@ -3,22 +3,22 @@
 #include "functions.h"
 
  TEST(expression, format) {
-	EXPECT_ANY_THROW(solution("()")); 
-    EXPECT_ANY_THROW(solution("(1"));
-    EXPECT_ANY_THROW(solution("("));
-    EXPECT_ANY_THROW(solution(""));
-    EXPECT_ANY_THROW(solution(")"));
-    EXPECT_ANY_THROW(solution("(1 + 3"));
-    EXPECT_ANY_THROW(solution("(1 + )3 + 4"));
-    EXPECT_ANY_THROW(solution("(1 + ((3 + 4)"));
-    EXPECT_ANY_THROW(solution("2 ** 2 ***3"));
-	EXPECT_NEAR(solution("(1)"), 1.0, 0.00001);
-	EXPECT_NEAR(solution("1 + 2 3"), 7.0, 0.00001);
-	EXPECT_NEAR(solution("(1 + 2)(3 + 4) (4+1)"), 105.0, 0.00001);
-	EXPECT_NEAR(solution("(1 + 2)7 (4+1)"), 105.0, 0.00001);
-	EXPECT_NEAR(solution("(1 + 2)(3 + 4)5"), 105.0, 0.00001);
-	EXPECT_NEAR(solution("7 (1   + 2)  / 7   * 5"), 15.0, 0.00001);
-	EXPECT_NEAR(solution("4"), 4.0, 0.00001);
-	EXPECT_NEAR(solution("2 + 2 * 2"), 6.0, 0.00001);
-	EXPECT_NEAR(solution("2 ** 2 ** 3"), 256.0, 0.00001);
+	EXPECT_ANY_THROW(solution<double>("()")); 
+    EXPECT_ANY_THROW(solution<double>("(1"));
+    EXPECT_ANY_THROW(solution<double>("("));
+    EXPECT_ANY_THROW(solution<double>(""));
+    EXPECT_ANY_THROW(solution<double>(")"));
+    EXPECT_ANY_THROW(solution<double>("(1 + 3"));
+    EXPECT_ANY_THROW(solution<double>("(1 + )3 + 4"));
+    EXPECT_ANY_THROW(solution<double>("(1 + ((3 + 4)"));
+    EXPECT_ANY_THROW(solution<double>("2 ** 2 ***3"));
+	EXPECT_NEAR(solution<double>("(1)"), 1.0, 0.00001);
+	EXPECT_NEAR(solution<double>("1 + 2 3"), 7.0, 0.00001);
+	EXPECT_NEAR(solution<double>("(1 + 2)(3 + 4) (4+1)"), 105.0, 0.00001);
+	EXPECT_NEAR(solution<double>("(1 + 2)7 (4+1)"), 105.0, 0.00001);
+	EXPECT_NEAR(solution<double>("(1 + 2)(3 + 4)5"), 105.0, 0.00001);
+	EXPECT_NEAR(solution<double>("7 (1   + 2)  / 7   * 5"), 15.0, 0.00001);
+	EXPECT_NEAR(solution<double>("4"), 4.0, 0.00001);
+	EXPECT_NEAR(solution<double>("2 + 2 * 2"), 6.0, 0.00001);
+	EXPECT_NEAR(solution<double>("2 ** 2 ** 3"), 256.0, 0.00001);
 }
